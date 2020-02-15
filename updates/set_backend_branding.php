3f2177987bf0581c73bca2f5e548709d4f1095a7
Joe Buonocore <joe@buonocoreco.com>
2020-02-15 02:12:48 -0500

    Make the branding update on install

---
+++ updates/set_backend_branding.php
@@ -0,0 +1,18 @@
+<?php namespace Albright\Base\Updates;
+
+use Db;
+use October\Rain\Database\Updates\Migration;
+
+class SetBackendBranding extends Migration
+{
+    public function up()
+    {
+      Db::table('system_settings')->insert(
+        ['item' => 'backend_brand_settings', 'value' => '{"app_name":"Albright Labs","app_tagline":"Albright Labs","primary_color":"#222222","secondary_color":"#009954","accent_color":"#3498db","menu_mode":"inline","custom_css":""}']
+      );
+    }
+
+    public function down(){
+      
+    }
+}
